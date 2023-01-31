ec1484352007b80ae9a537b39d7e947e6202ee81
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-01-31 14:36:39 -0600

    WIP

--- fortuna-terminal/comm/uart.cc
+++ fortuna-terminal/comm/uart.cc
@@ -1,4 +1,5 @@
 #include "uart.hh"
+#include "debug.hh"
 
 #include <cerrno>
 #include <cstdio>
@@ -12,6 +13,62 @@
 
 void UART::initialize()
 {
+    fd = open(serial_options_.port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd < 0) {
+        error_message("Error opening serial port: ", true);
+        return;
+    }
+    if (debug_mode)
+        std::cout << "Serial port initialized.\n";
+
+    fcntl(fd, F_SETFL, 0);
+
+    struct termios options;
+    if (tcgetattr (fd, &options) != 0)
+    {
+        error_message("Could not get current serial attributes: ", true);
+        return;
+    }
+
+    if (cfsetspeed(&options, (speed_t) serial_options_.baud) < 0) {
+        error_message("Could not set current serial baud speed: ", true);
+        return;
+    }
+
+    options.c_cflag |= (CLOCAL | CREAD);  // enable received and set local mode
+
+    options.c_cflag &= ~CSIZE; /* Mask the character size bits */
+    options.c_cflag |= CS8; /* Select 8 data bits */
+
+    if (serial_options_.parity == 'N') {
+        options.c_cflag &= ~PARENB;
+    } else if (serial_options_.parity == 'O') {
+        options.c_cflag |= PARENB;
+        options.c_cflag |= PARODD;
+    } else if (serial_options_.parity == 'E') {
+        options.c_cflag |= PARENB;
+        options.c_cflag &= ~PARODD;
+    }
+
+    if (serial_options_.stop_bits == 2)
+        options.c_cflag |= CSTOPB;
+    else
+        options.c_cflag &= ~CSTOPB;
+
+    options.c_cflag &= ~CRTSCTS;  // disable flow control
+
+    options.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    options.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    options.c_oflag &= ~OPOST;
+
+    if (tcsetattr (fd, TCSANOW, &options) != 0)
+    {
+        error_message("Could not set current serial attributes: ", true);
+        return;
+    }
+
+    if (debug_mode)
+        printf("Serial port configured.\n");
 
 }
 
