e2efa142e1f0c3624b272e2163a168dd2206287f
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-02-02 16:06:48 -0600

    WIP

--- fortuna-terminal/comm/comm_module.cc
+++ fortuna-terminal/comm/comm_module.cc
@@ -66,7 +66,7 @@ void CommunicationModule::debug_received_byte(uint8_t byte) const
         if (byte >= 32 && byte < 127)
             std::cout << "[\e[0;32m" << (char) byte << "\e[0m] " << std::flush;
         else
-            std::cout << "[\e[0;32m" << std::hex << std::setw(2) << std::setfill('0') << byte << "\e[0m] " << std::flush;
+            std::cout << "[\e[0;32m" << std::hex << std::setw(2) << std::setfill('0') << (int) byte << "\e[0m] " << std::flush;
     }
 }
 
@@ -76,6 +76,6 @@ void CommunicationModule::debug_sent_byte(uint8_t byte) const
         if (byte >= 32 && byte < 127)
             std::cout << "'\e[0;31m" << (char) byte << "\e[0m' " << std::flush;
         else
-            std::cout << "\e[0;31m" << std::hex << std::setw(2) << std::setfill('0') << byte << "\e[0m " << std::flush;
+            std::cout << "\e[0;31m" << std::hex << std::setw(2) << std::setfill('0') << (int) byte << "\e[0m " << std::flush;
     }
 }
