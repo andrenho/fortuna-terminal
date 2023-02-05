651dfaac7ae0b1293f5569a7a51d60a75e900d74
André Wagner <andre.nho@gmail.com>
2023-02-05 12:42:58 -0600

    WIP

--- test-circuit/main.c
+++ test-circuit/main.c
@@ -1,20 +1,12 @@
 #include "uart.h"
-#include "i2c.h"
 
 #include <stdio.h>
 
 int main(void) {
-	i2c_init();
-	i2c_start();
-    i2c_send_byte('H');
-    i2c_stop();
-
-	/*
     uart_init();
 
     for (size_t i = 0; i < 900; ++i)
         putchar('x');
-	*/
 
     for (;;);
 }
