369350dbc57b3421589964d4365539300276f1df
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-02-11 00:32:45 -0600

    WIP

--- libfortuna-client/libfortuna.c
+++ libfortuna-client/libfortuna.c
@@ -1,20 +1,33 @@
 #include "libfortuna.h"
 
 typedef struct FortunaTerminal {
-    void(* receive_data)(FP_Command* cmds, size_t sz);
+    int    (* send)(uint8_t* bytes, size_t sz);
+    ssize_t(* recv)(uint8_t* bytes, size_t max_sz);
     void* data;
 } FortunaTerminal;
 
-void ft_init(FortunaTerminal* ft, void(* receive_data)(FP_Command* cmds, size_t sz), void* data)
+void ft_init(FortunaTerminal* ft,
+             int    (* send)(uint8_t* bytes, size_t sz),
+             ssize_t(* recv)(uint8_t* bytes, size_t max_sz),
+             void* data)
 {
-    ft->receive_data = receive_data;
+    ft->send = send;
+    ft->recv = recv;
     ft->data = data;
-}
-
-void ft_send(FP_Command* cmds, size_t sz)
-{
 }
 
 void ft_finalize(FortunaTerminal* ft)
 {
+    (void) ft;
 }
+
+int ft_send(struct FortunaTerminal* ft, FP_Command cmds[], size_t sz)
+{
+    return 0;
+}
+
+int ft_recv(struct FortunaTerminal* ft, FP_Command cmds[], size_t max_sz)
+{
+    return 0;
+}
+
--- libfortuna-client/libfortuna.h
+++ libfortuna-client/libfortuna.h
@@ -7,8 +7,13 @@
 
 struct FortunaTerminal;
 
-void ft_init(struct FortunaTerminal* ft, void(* receive_data)(FP_Command* cmds, size_t sz), void* data);
-void ft_send(struct FortunaTerminal* ft, FP_Command* cmds, size_t sz);
+void ft_init(struct FortunaTerminal* ft,
+             int    (* send)(uint8_t* bytes, size_t sz),
+             ssize_t(* recv)(uint8_t* bytes, size_t max_sz),
+             void* data);
 void ft_finalize(struct FortunaTerminal* ft);
+
+int ft_send(struct FortunaTerminal* ft, FP_Command cmds[], size_t sz);
+int ft_recv(struct FortunaTerminal* ft, FP_Command cmds[], size_t max_sz);
 
 #endif //LIBFORTUNA_H_
