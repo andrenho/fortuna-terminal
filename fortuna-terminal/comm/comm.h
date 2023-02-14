a2dd825406bf6b65976e4129d123cf7b2a319e8d
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-02-13 18:27:27 -0600

    .

--- fortuna-terminal/comm/comm.h
+++ fortuna-terminal/comm/comm.h
@@ -7,8 +7,6 @@
 int    comm_init(size_t lines, size_t columns);
 int    comm_run_input(Buffer* input_buffer);
 int    comm_run_output(Buffer* output_buffer);
-size_t comm_unload_input_queue(uint8_t* dest, size_t max_sz);
-int    comm_add_to_output_queue(uint8_t* data, uint8_t sz);
 int    comm_notify_vsync();
 int    comm_finalize();
 
---
+++ fortuna-terminal/temp-refactor/SERVICES.md
@@ -0,0 +1,9 @@
+- Enviroments contain
+  - Input & output buffer
+  - A single scene
+  - A communication module
+
+- Environments share
+  - Error handling (?)
+  - Protocols (functions should have no side effects)
+  - The UI
