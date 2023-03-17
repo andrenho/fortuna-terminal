f60dcf22d48cbdf936bcabdc748a69ecafc28491
André Wagner <andre.nho@gmail.com>
2023-03-17 17:04:33 -0500

    WIP

--- fortuna-terminal/env/comm/xchg/spi.cc
+++ fortuna-terminal/env/comm/xchg/spi.cc
@@ -1,6 +1,9 @@
 #include "spi.hh"
 
+#include <thread>
 #include <pigpio.h>
+
+using namespace std::chrono_literals;
 
 SPI::SPI(SPIOptions spi_options)
 {
@@ -15,20 +18,24 @@ SPI::~SPI()
 std::vector<uint8_t> SPI::exchange(std::vector<uint8_t> const &data)
 {
     std::vector<uint8_t> rx_vec;
+    uint8_t tx, rx;
+
     size_t i = 0;
+
     do {
-        uint8_t tx = 0xff;
+        tx = 0xff;
         if (i < data.size())
             tx = data.at(i);
 
-        uint8_t rx;
-        spiXfer(handle_, &tx, &rx, 1);
+        spiXfer(handle_, (char *) &tx, (char *) &rx, 1);
         if (rx != 0xff)
             rx_vec.push_back(rx);
 
-        ++i;
-    } while (tx != 0xff || rx != 0xff);
+        std::this_thread::sleep_for(10us);
 
-    return rx_buf;
+        ++i;
+    } while (!(tx == 0xff && rx == 0xff));
+
+    return rx_vec;
 }
 
