fe15b0ac04bef3e3047df4b2a40962979068b369
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-03-18 19:12:10 -0500

    .

--- fortuna-terminal/application/gpio.cc
+++ fortuna-terminal/application/gpio.cc
@@ -5,11 +5,21 @@
 #  include <pigpio.h>
 #endif
 
+using namespace std::chrono_literals;
+
 Gpio::Gpio()
 {
 #ifdef GPIO
     if (gpioInitialise() < 0)
         throw FortunaException("Failed to intialize pigpio.");
+
+    gpioSetMode(PIN_VSYNC, PI_OUTPUT);
+    gpioWrite(PIN_VSYNC, 1);
+
+    gpioSetMode(PIN_RESET, PI_OUTPUT);
+    gpioSetPullUpDown(PIN_RESET, PI_PUD_UP);
+
+    vsync_thread_.run_with_wait([this]{ gpioTrigger(PIN_VSYNC, 100, 0); });
 #endif
 }
 
@@ -17,6 +27,23 @@ Gpio::~Gpio()
 {
 #ifdef GPIO
     gpioTerminate();
+#endif
+}
+
+void Gpio::reset()
+{
+#ifdef GPIO
+    gpioSetPullUpDown(PIN_RESET, PI_PUD_OFF);
+    gpioWrite(PIN_RESET, 0);
+    std::this_thread::sleep_for(200ms);
+    gpioSetPullUpDown(PIN_RESET, PI_PUD_UP);
+#endif
+}
+
+void Gpio::vsync()
+{
+#ifdef GPIO
+    vsync_thread_.notify();
 #endif
 }
 
--- fortuna-terminal/application/gpio.hh
+++ fortuna-terminal/application/gpio.hh
@@ -1,13 +1,21 @@
 #ifndef GPIO_HH_
 #define GPIO_HH_
 
+#include "common/iterativethread.hh"
+
 class Gpio {
 public:
     Gpio();
     ~Gpio();
 
-    void reset() {}
-    void vsync() {}
+    void reset();
+    void vsync();
+
+private:
+    IterativeThread vsync_thread_;
+
+    static constexpr unsigned PIN_VSYNC = 27;
+    static constexpr unsigned PIN_RESET = 25;
 };
 
 #endif //GPIO_HH_
