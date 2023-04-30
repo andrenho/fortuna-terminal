9e6e064a24188642d99895d2cce74c794124c121
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-04-29 19:12:24 -0500

    .

--- fortuna-terminal/tests/tests.cc
+++ fortuna-terminal/tests/tests.cc
@@ -10,15 +10,21 @@
 static void validate_varint(std::vector<int> const &values, size_t expected_count)
 {
     std::vector<uint8_t> str = to_varint(values);
-    auto [count, result] = from_varint(std::span<uint8_t>{ str }, values.size());
+    auto [count, result] = from_varint(str, values.size());
     if (values.size() != result.size() || !std::equal(values.begin(), values.end(), result.begin()))
         FAIL();
     if (count != expected_count)
         FAIL();
 }
 
+static void compare_from_varint(std::vector<uint8_t> const& bytes, size_t expected_count, std::vector<int> const& expected)
+{
+    auto [count, result] = from_varint(std::span<const uint8_t> { bytes }, 5);
+}
+
 int main()
 {
+    // varint
     validate_varint({0}, 1);
     validate_varint({200}, 2);
     validate_varint({10000}, 2);
@@ -26,7 +32,9 @@ int main()
     validate_varint({-200}, 2);
     validate_varint({-10000}, 2);
 
-    // TODO: rle
+    // RLE
+    compare_from_varint({ 0xff, 0x05, 0xab }, 5, { 0xab, 0xab, 0xab, 0xab, 0xab });
+
     // TODO: incomplete request
 
     return EXIT_SUCCESS;
